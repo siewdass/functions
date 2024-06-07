d289c61e9899a4ae27a06420594df874ce3c927e
Samuel Siewdass <samuel.siewdass@itsense.com.co>
2024-06-06 21:26:13 -0400

    x

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = function() {
-    return Math.floor(Math.random() * 10)
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Test;
+function Test() {
+  return Math.floor(Math.random() * 10);
 }
