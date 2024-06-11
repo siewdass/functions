3d6bd63720d71b53c6cc3d9f5f201b7f6503922e
Samuel Siewdass <samuel.siewdass@itsense.com.co>
2024-06-10 20:37:07 -0400

    x

--- home.js
+++ home.js
@@ -1,6 +1,11 @@
 export function home( components ) {
-    const handler = () => {
-        console.log('sss')
+    const handler = async () => {
+        const response = await fetch( 'http://woveer.ddns.net/api/testing', {
+            method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body:JSON.stringify( { daa: 2 } )
+		} )
+        console.log( response.json( ) )
     }
     return (
         <div>
