4bdd7c47387597674e06c1fc1d4f6f3e28a2e73a
Samuel Siewdass <samuel.siewdass@itsense.com.co>
2024-06-11 22:06:40 -0400

    x

--- home.js
+++ home.js
@@ -1,4 +1,6 @@
 export function home( { useHistory } ) {
+    const history = useHistory()
+
     const handler = async () => {
         const response = await fetch( 'http://woveer.ddns.net:3000/api/tester/register', {
             method: 'POST',
@@ -11,7 +13,7 @@ export function home( { useHistory } ) {
         <div>
             <h3>Home</h3>
             <button onClick={ handler }>Click</button>
-            <button onClick={ ( ) => console.log( 'Buenasxxxdx', useHistory ) }>GoTo</button>
+            <button onClick={ ( ) => history.push('/about') }>GoTo</button>
         </div>
     )
 }
