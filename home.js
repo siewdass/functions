dfbb0777c857e0b632462f7b888a71c0d82ec31e
Samuel Siewdass <samuel.siewdass@itsense.com.co>
2024-06-14 12:31:38 -0400

    x

--- home.js
+++ home.js
@@ -1,18 +1,10 @@
-export function home( { useNavigate } ) {
+export function home( { useNavigate, useRequest } ) {
     const navigate = useNavigate()
-    const handler = async () => {
-        const response = await fetch( 'http://woveer.ddns.net:3000/api/tester/register', {
-            method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify( { daa: 2 } )
-		} )
-        console.log( await response.json( ) )
-    }
-    
+
     return (
         <div>
             <h3>Home</h3>
-            <button onClick={ handler }>Click</button>
+            <button onClick={ () => useRequest('POST', 'http://woveer.ddns.net:3000/api/tester/register', { daa: 2 } ) }>Click</button>
             <button onClick={ ( ) => navigate( '/about' ) }>GoTo</button>
         </div>
     )
