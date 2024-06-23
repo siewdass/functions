bc8d27d04a6f0a1966e2b28d4796c7e020b437ed
Samuel Siewdass <samuel.siewdass@itsense.com.co>
2024-06-23 19:57:37 -0400

    x

--- register.js
+++ register.js
@@ -1,7 +1,5 @@
 export async function register( { database } ) {
   const collection = database.collection('Beheart');
   const results = await collection.find({}).toArray();
-  console.log(results)
-
-  return ['asdasd',2]
+  return await collection.find({}).toArray()
 }
