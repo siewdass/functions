8edf34f74119475430bc708751283599434cf172
Samuel Siewdass <samuel.siewdass@itsense.com.co>
2024-06-23 19:06:26 -0400

    x

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 export function home( { useNavigate, useFetch } ) {
     const navigate = useNavigate()
     const handler = async () => {
-        const response = await useFetch('POST', 'http://riser.ddns.net:3000/api/cc36baaf-a634-474f-8b02-37e95b7cf8dd/register', { daa: 2 } )
+        const response = await useFetch('POST', '/register', { daa: 2 } )
         console.log( response )
     }
     
